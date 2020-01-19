427f77726fc3d31662d3ba941f8852aa52e3eb43
SimoLieF <55975272+SimoLieF@users.noreply.github.com>
2020-01-20 05:11:43 +0900

    Update main.c

--- main.c
+++ main.c
@@ -20,11 +20,11 @@ int main(int argc, char *argv[])
         //printf("SSS_%d_SSS\n",seed);
         //printf("%s", str);
     x=seed;
-    for(int i=0;i<num;i++){
+    //for(int i=0;i<num;i++){
        x=(48271*x)%(2147483647 - 1);
        x=x/((2147483647+max-1)/max)+1;
        printf("%d\n",x);
-    }
+    //}
 
     return 0;
 }
